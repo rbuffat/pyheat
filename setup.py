aa32beec20cc9b7405edd6f8e274a71f51f2694a
René Buffat <buffat@gmail.com>
2020-06-01 20:01:59 +0200

    disable cython

--- setup.py
+++ setup.py
@@ -8,16 +8,16 @@ except ImportError:
 else:
     use_cython = True
 
-ext_modules = []
-if use_cython:
-    extensions = [
-        Extension("pyheat.sia380cy", ['pyheat/sia380cy.pyx']),
-    ]
-    ext_modules = cythonize(extensions)
+# ext_modules = []
+# if use_cython:
+#     extensions = [
+#         Extension("pyheat.sia380cy", ['pyheat/sia380cy.pyx']),
+#     ]
+#     ext_modules = cythonize(extensions)
 
 setup(
     name='pyheat',
     version="0.3.dev",
-    ext_modules=ext_modules,
+    # ext_modules=ext_modules,
     install_requires=['numpy']
 )
